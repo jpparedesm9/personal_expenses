0f7e63505a175b5f01902b85fda0c5a4436273d6
FlutLab.io <>
2021-06-04 13:53:25 +0000

    Switching to branch dfg

--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class UserTransactions extends StatefulWidget {
+  @override
+  _UserTransactionsState createState() => _UserTransactionsState();
+}
+
+class _UserTransactionsState extends State<UserTransactions> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
